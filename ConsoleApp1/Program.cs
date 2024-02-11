95f1f647ee8beba7ee265b9484f9d9d7fefe155d
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2024-02-11 17:28:10 +0000

    Getters & Setters

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -194,11 +194,16 @@ namespace ConsoleApp1
             //Console.ReadLine();
 
             // Object methods
-            Student student1 = new Student("Jim", "Business", 2.8);
-            Student student2 = new Student("Pam", "Art", 3.6);
+            //Student student1 = new Student("Jim", "Business", 2.8);
+            //Student student2 = new Student("Pam", "Art", 3.6);
 
-            Console.WriteLine(student1.HasHonors());
-            Console.WriteLine(student2.HasHonors());
+            //Console.WriteLine(student1.HasHonors());
+            //Console.WriteLine(student2.HasHonors());
+
+            // Getters & Setters
+            Movie avengers = new Movie("The Avengers", "Joss Whedon", "test");
+            Movie shreck = new Movie("Shreck", "Adam Adamson", "PG");
+            Console.WriteLine(avengers.Rating);
 
             Console.ReadLine();
         }
